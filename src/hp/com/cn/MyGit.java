035700b6f547d4c635d80ee46f9eefa96d669ac6
MrWangYafei <mryafeiwang@126.com>
2019-12-23 21:10:25 +0800

    IDEA提交代码2

--- src/hp/com/cn/MyGit.java
+++ src/hp/com/cn/MyGit.java
@@ -2,6 +2,7 @@ package hp.com.cn;
 
 public class MyGit {
     public static void main(String[] args) {
-        System.out.println("IDEAMyGit");
+        System.out.println("IDEA MyGit");
+        System.out.println("lzh");
     }
 }
